2c300366a6682822463349bb24e8605bc392ccd9
macik <macikucia@onet.pl>
2024-11-28 13:52:10 +0100

    swagger metadata

--- RestApiRecruitmentTask/Program.cs
+++ RestApiRecruitmentTask/Program.cs
@@ -8,7 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen((c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "sklepopon API",
+        Version = "v1",
+        Description = "API for managing tires and producers."
+    });
+}));
 builder.Services.AddSingleton<IProducerService, ProducerService>(); // to save data in memory
 builder.Services.AddSingleton<ITireService, TireService>();
 builder.Services.AddAutoMapper(typeof(Program));
